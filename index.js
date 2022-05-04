70cb3db789222ba54f3dda52f14d37276c170c68
ET45 <emre.tugutlu@gmail.com>
2022-05-04 13:42:47 +0300

    getting my data

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 const express = require("express");
 
+const Game = require("./models/").game;
+
 const app = express();
 
 const port = 4000;
 
 app.listen(port, () => console.log(`Listening on :${port}`));
+
+app.get("/", async (req, res) => {
+  try {
+    const allGames = await Game.findAll();
+    res.status(200).send(allGames);
+  } catch (e) {
+    console.log("error", e);
+  }
+});
